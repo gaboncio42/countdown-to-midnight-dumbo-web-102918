5729d9c01302d3da69a95052fdbeab2f364e9d3b
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-10 22:25:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,4 @@
 #write your code here
 
-def countdown(number)
-  number = 10
-  while number == 0
-    puts "-#{number} SECOND(S)!"
-    number -= 1
-  end
-  return "HAPPY NEW YEAR!"
+def countdown
 end
